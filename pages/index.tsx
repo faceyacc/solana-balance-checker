c1e5c40e15bc12fb6583d53aa4ed18f53cbf7b7d
Ty <justfacey@gmail.com>
2022-11-07 16:23:06 -0800

    added executable field

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import * as Web3 from '@solana/web3.js'
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [executable, setExecutable] = useState('')
 
   const addressSubmittedHandler = (address: string) => {
     try {
@@ -15,6 +16,11 @@ const Home: NextPage = () => {
       const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
       connection.getBalance(key).then(balance => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
+
+      connection.getAccountInfo(key).then(res => {
+
+        setExecutable(res?.executable ?? false)
       })
     } catch (error) {
       setAddress('')
@@ -32,6 +38,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`It is executable?: ${executable ? 'Yep':'Nope'}`}</p>
       </header>
     </div>
   )
